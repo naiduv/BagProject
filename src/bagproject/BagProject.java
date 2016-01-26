1ace822686fb4428f15c25f9df5dd0b34bb58f1d
vnaidu <vnaidu@WS-AC-CTD-370IN.suscorp.edu>
2016-01-25 19:51:58 -0500

    added some stuff to IntegerBag

--- src/bagproject/BagProject.java
+++ src/bagproject/BagProject.java
@@ -16,31 +16,41 @@ public class BagProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello");   
+        TestIntegerBag();
         
+    }
+    
+    public static void TestIntegerBag(){
         IntegerBag myBag = new IntegerBag();
         
         myBag.add(10);
+        myBag.add(11);
         myBag.add(12);
-        myBag.add(10);
-        myBag.add(12);
-        myBag.add(10);
-        myBag.add(12);
-        myBag.add(10);
-        myBag.add(12);
-        myBag.add(10);
+        myBag.add(13);
+        myBag.add(14);
+        myBag.add(15);
+        myBag.add(16);
+        myBag.add(17);
+        myBag.add(18);
         myBag.add(12);
         myBag.add(10);
         myBag.add(12);   
         
         myBag.remove();
         myBag.remove();
-        myBag.remove();
-        myBag.remove();
-        myBag.remove();
         
-        myBag.clear();
+        System.out.println(myBag);
+        myBag.removeAt(3);
+        System.out.println(myBag);
+        myBag.removeAt(0);
         
+        
+        System.out.println(myBag);
+        
+        myBag.removeItem(11);
+        System.out.println(myBag);
+        
+        myBag.removeItem(111);
     }
     
 }
